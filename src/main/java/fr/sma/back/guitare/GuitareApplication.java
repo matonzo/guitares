f91f4a4c4ea3d76a02c4bfbbdfda9489a282a6df
smasson <sylvain.masson@soprasteria.com>
2022-07-08 14:54:06 +0200

    Initial commit

---
+++ src/main/java/fr/sma/back/guitare/GuitareApplication.java
@@ -0,0 +1,13 @@
+package fr.sma.back.guitare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuitareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/sma/back/guitare/GuitareApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sma.back.guitare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
